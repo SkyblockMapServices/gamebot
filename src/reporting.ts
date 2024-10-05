aa80ee981ff23566ca04d6ab2106c35e0cead36b
AnotherPillow <85362273+AnotherPillow@users.noreply.github.com>
2024-10-05 20:16:06 +0100

    Fix reporting typo

--- src/reporting.ts
+++ src/reporting.ts
@@ -3,7 +3,7 @@ import Logger from '../../discord-bot/src/Logger'
 export type SimpleReportedInventory = ({name: string, count: number} | null)[]
 
 export const URLs = {
-    placement: process.env.REPORTING_HOSTNAME + '/gamebot/reporting_placing_milestone'
+    placement: process.env.REPORTING_HOSTNAME + '/gamebot/report_placing_milestone'
 }
 
 export const announceMilestonePlacing = (n: number, coords: [number, number, number][], inventory: SimpleReportedInventory) => {
